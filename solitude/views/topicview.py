c04eba455b06d1bbd692c935dd92b99a07265231
tolgafiratoglu <tolgafiratoglu@gmail.com>
2022-12-05 17:47:50 +0300

    bad status for invalid data

--- solitude/views/topicview.py
+++ solitude/views/topicview.py
@@ -6,25 +6,26 @@ from solitude.services.clusterservice import ClusterService
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
 
 class TopicView(viewsets.ViewSet):
     permission_classes = (IsAuthenticated, IsAdminUser, )
     authentication_classes = (JWTAuthentication, )
 
     def save_topic(self, request, cluster_id):
-        topic_title = request.POST.get('topic_title')
-        topic_partition = int(request.POST.get('topic_partition_number'))
-        topic_replication_factor = int(request.POST.get('topic_replication_factor'))
+        topic_title = request.POST.get('topic_title', '')
+        topic_partition = int(request.POST.get('topic_partition_number', ''))
+        topic_replication_factor =int(request.POST.get('topic_replication_factor', ''))
+        if topic_title == '' or topic_partition == 0 or topic_replication_factor == 0:
+            return HttpResponseBadRequest(JsonResponse({"status": "error", "message": "Please provide valid values"}))
         bootstrap_servers = ClusterService.get_cluster_brokers(cluster_id)
         if bootstrap_servers is not None:
             try:
                 KafkaAdminService.create_topic(bootstrap_servers, topic_title=topic_title, partition_number=topic_partition, replication_factor=topic_replication_factor)
                 return JsonResponse({'status': 'success'})
             except Exception as e:
-                return JsonResponse({'status': 'error', 'message': str(e)})
-        else:       
-            return JsonResponse({'status': 'error', 'message': "Broker doesn't have a valid host"})
+                return HttpResponseBadRequest(JsonResponse({'status': 'error', 'message': str(e)}))
+        return JsonResponse({'status': 'error', 'message': "Cluster doesn't have any broker"})
 
     def create_partials(self, request, cluster_id, topic):
         bootstrap_servers = ClusterService.get_cluster_brokers(cluster_id)
