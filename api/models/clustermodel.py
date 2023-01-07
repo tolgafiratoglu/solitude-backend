eba8c2f26b892358ecb0d28b02861fee21c7e541
tolgafiratoglu <tolgafiratoglu@gmail.com>
2023-01-07 18:14:17 +0300

    broker, cluster models

--- api/models/clustermodel.py
+++ api/models/clustermodel.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 class Cluster(models.Model):
     managed = True
-    created_by = models.ForeignKey(get_user_model(), unique=False, editable=False, on_delete=models.DO_NOTHING, default=None, blank=True, null=True)
+    created_by = models.ForeignKey(get_user_model(), unique=False, editable=False, on_delete=models.DO_NOTHING, default=None, blank=False, null=False)
     title = models.CharField(max_length=255, default='', null=False, blank=False)
     active = models.BooleanField(default=True)
     notes = models.TextField('Notes', default='', null=True, blank=True)
