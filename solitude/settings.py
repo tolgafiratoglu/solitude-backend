2c1d2b93c04d6db2835d69883bcb9bba4e4b366c
tolgafiratoglu <tolgafiratoglu@gmail.com>
2023-01-08 21:25:10 +0300

    removed secret from settings, added .env file

--- solitude/settings.py
+++ solitude/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import datetime
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z*r+duhe0#$ow0h%$$ap%8sty2pwc#03^(t=qnrgg@swsz*o0*'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
